3b490cd49b2b645278b6757dfd147a4768e145c7
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-07-26 19:40:22 +0500

    files add

---
+++ app.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://hwyofckifjeewvmagsgc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3eW9mY2tpZmplZXd2bWFnc2djIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0Mzk0MDcsImV4cCI6MjA2NzAxNTQwN30.pvsjYctkRI7NHdJrSuatOb-oLoatl8dIZfHUKv74gvE";
+
+const { createClient } = supabase;
+const client = createClient(supabaseUrl, supabaseKey);
+console.log(createClient);
