66123be3ab4e01a1fb569092581961643cffa66e
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-08-02 02:42:22 +0500

    issue page not redirect

--- app.js
+++ app.js
@@ -7,42 +7,113 @@ const client = createClient(supabaseUrl, supabaseKey);
 console.log(createClient);
 
 // Signup Functionality
-const signUp = document.getElementById("signUp")
+const signUp = document.getElementById("signUp");
 
 // use addEventListener
 
-signUp && signUp.addEventListener("click" , async () => {
-  // Getting user details and store in variables
-  const fullName = document.getElementById("fullName")
-  console.log(fullName);
-  const emailAdress = document.getElementById("emailAdress")
-  console.log(emailAdress);
-  const userPassword = document.getElementById("userPassword")
-  console.log(userPassword);
-  const userProfilePic = document.getElementById("userProfilePic")
-  console.log(userProfilePic);
+signUp &&
+  signUp.addEventListener("click", async () => {
+    // Getting user details and store in variables
+    const fullName = document.getElementById("fullName").value;
+    const emailAdress = document.getElementById("emailAdress").value;
+    const userPassword = document.getElementById("userPassword").value;
+    const userProfilePic = document.getElementById("userProfilePic");
 
-  // Getting User Data and destructuring
-  const {data : { user }} = await client.auth.getUser()
-  console.log(user);
+    // Getting User Data and destructuring
+    const {
+      data: { user },
+    } = await client.auth.getUser();
+    console.log(user);
 
-  // Set file extension for saving, update and delete
-  const fileExt = userProfilePic.files[0].name.split('.')[1]
-  console.log(fileExt);
-  
-  // Signup Authentication
-  if(emailAdress && userPassword){
-    try{
-      // Again getting user data when user signup account
-      const {data : { user } , error} = await client.auth.signUp(
-       {
-        // Set value of user email and password by using destructuring method
-        email : emailAdress.value , 
-        password : userPassword.value
-      })
+    // Set file extension for saving, update and delete
+    const fileExt = userProfilePic.files[0].name.split(".")[1];
+    console.log(fileExt);
 
-    }catch{
+    // Signup Authentication
+    if (emailAdress && userPassword) {
+      try {
+        // Again getting user data when user signup account
+        const {
+          data: { user },
+          error,
+        } = await client.auth.signUp({
+          // Set value of user email and password by using destructuring method
+          email: emailAdress,
+          password: userPassword,
+        });
+        if (user) {
+          console.log(user);
+          // Destructuring Profile data name
+          const { data: profileUrl, error } = await client.storage.from('profilepic')
+          .upload(`Avatars/user-${user.id}.${fileExt}` , userProfilePic.files[0],{
+            upsert: true 
+          });
+          if(error){
+            console.log(error);
+          }else{
+            console.log("upload data:" , profileUrl);
 
+            // Getting Profile URL
+            const {data : {pulicUrl}} = client.storage.from('profilepic').getPublicUrl(`Avatars/user-${user.id}.${fileExt}`)
+            console.log(pulicUrl);
+          }
+        }
+      } catch (error){
+        console.log("Signup Error:" , error);
+        if(error.message.includes("invalid format")){
+          alert("Please enter valid format")
+        }
+      }
+    }else{
+      if(email){
+        alert("Please enter password")
+      }else{
+        alert("please enter email")
+      }
     }
+  });
+
+
+// Fetch Profile Picture and User Details
+
+// creating function SHOW DETAILS
+
+const showDetails = async (profilepic , email , fullName) => {
+  // Nested destructuring 
+  const {data: { user: {id: userId , email: userEmail} } , error} = await client.auth.getUser()
+  if(email){
+    email.value = userEmail
   }
-})
+  if(userId){
+    const{data: [{fullName: dbName , profilepic: pulicUrl}] , error} = await client.from('users_profile').select()
+    .eq('user_id' , userId)
+
+    console.log(fullName , profilepic);
+    if(pulicUrl){
+      const avatar = document.getElementById("avatar")
+      if(avatar){
+        avatar.src = pulicUrl
+      }
+      if(profilepic){
+        profilepic.src = pulicUrl
+        fullName.value = dbName
+      }
+    }else{
+      console.log(error);
+    }
+  }else{
+    console.log(error);
+  }
+}
+// Page direction when user details show
+if(window.location.pathname == '/post.html'){
+  showDetails()
+}
+
+if(window.location.pathname == '/profile.html'){
+  const profilepic = document.getElementById("profilepic")
+  const email = document.getElementById("email")
+  const full_name = document.getElementById("full_name")
+  console.log(profilepic , email , full_name);
+  showDetails(profilepic , email , full_name)
+}
